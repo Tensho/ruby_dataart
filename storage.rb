e482d6f7bde16087fe568d89b0fef3c672204dfe
Andrew Babichev <ababichev@dataart.com>
2014-10-13 13:20:50 +0300

    Storage adds strings

--- storage.rb
+++ storage.rb
@@ -1,2 +1,38 @@
 class Storage
+
+  class Node
+    attr_reader :value
+    attr_accessor :children
+
+    def initialize(value, children = [])
+      @value = value
+      @children = children
+    end
+  end
+
+  attr_reader :root_node
+
+  def initialize
+    @root_node = Node.new('')
+  end
+
+  def add(str)
+    words = str.split(', ')
+    add_node(root_node, words) unless words.empty?
+  end
+
+  def add_node(node, words)
+    word = words.shift
+    child_node = node.children.detect { |child| child.value == word }
+    unless child_node
+      new_node = Node.new(word)
+      node.children << new_node
+      unless words.empty?
+        add_node(new_node, words)
+      end
+    else
+      add_node(child_node, words)
+    end
+  end
+
 end
--- storage_spec.rb
+++ storage_spec.rb
@@ -6,11 +6,26 @@ describe 'Storage (implements trie data structure)' do
 
   describe '#add' do
     context 'empty string' do
-      it 'root node has not children'
+      let(:str) { '' }
+
+      it 'root node has not children' do
+        subject.add(str)
+        subject.root_node.children.must_be_empty
+      end
     end
 
     context 'nonempty string' do
-      it 'root node has children'
+      let(:str_1) { 'abc, def, xyz' }
+      let(:str_2) { 'abc, def, ghi' }
+
+      it 'root node has children' do
+        subject.add(str_1)
+        subject.add(str_2)
+        subject.root_node.children.wont_be_empty
+        # TODO: subject.traverse(root_node)
+        subject.root_node.children.map(&:value).must_be :==, ['abc']
+        subject.root_node.children.flat_map(&:children).flat_map(&:value).must_be :==, ['def']
+      end
     end
   end
 
