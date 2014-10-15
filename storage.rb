60b579c6acc306bbff4a2450466f3f2e1c6d4e66
Andrew Babichev <ababichev@dataart.com>
2014-10-15 18:45:42 +0300

    Hide private under the hood

--- storage.rb
+++ storage.rb
@@ -33,6 +33,26 @@ class Storage
     self
   end
 
+  def contains?(word)
+    letters = word.split('')
+    letters.empty? ? true : _contains?(root_node, letters)
+  end
+
+  def find(prefix)
+    letters = prefix.split('')
+    raise ArgumentError, 'Prefix should be 3 letter at least' if letters.size < 3
+    _find(root_node, letters)
+  end
+
+  def to_h(node = root_node, res = {})
+    key = node.value + (node.terminal? ? "(X)" : "")
+    res[key] = {}
+    node.children.each { |child_node| to_h(child_node, res[key]) }
+    res
+  end
+
+  private
+
   def _add(node, values)
     value = values.shift
     child_node = node.children.detect { |child| child.value == value }
@@ -48,11 +68,6 @@ class Storage
     end
   end
 
-  def contains?(word)
-    letters = word.split('')
-    letters.empty? ? true : _contains?(root_node, letters)
-  end
-
   def _contains?(node, values)
     value = values.shift
     child_node = node.children.detect { |child| child.value == value }
@@ -61,12 +76,6 @@ class Storage
     else
       false
     end
-  end
-
-  def find(prefix)
-    letters = prefix.split('')
-    raise ArgumentError, 'Prefix should be 3 letter at least' if letters.size < 3
-    _find(root_node, letters)
   end
 
   def _find(node, values, res = [], s = '')
@@ -90,11 +99,5 @@ class Storage
     res
   end
 
-  def to_h(node = root_node, res = {})
-    key = node.value + (node.terminal? ? "(X)" : "")
-    res[key] = {}
-    node.children.each { |child_node| to_h(child_node, res[key]) }
-    res
-  end
 
 end
