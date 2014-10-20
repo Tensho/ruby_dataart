5c3ae13d26f3bde248af7c9bbc689f8908b01219
Andrew Babichev <ababichev@dataart.com>
2014-10-20 16:05:01 +0300

    Web storage within sinatra

---
+++ web_storage.rb
@@ -0,0 +1,20 @@
+require_relative 'storage'
+require 'sinatra'
+require 'json'
+
+s = Storage.new
+
+get '/add' do
+  content_type :json
+  s.add(params[:string]).to_h.to_json
+end
+
+get '/contains' do
+  content_type :json
+  { params[:string] => s.contains?(params[:string]) }.to_json
+end
+
+get '/find' do
+  content_type :json
+  s.find(params[:string]).to_json
+end
